e1d881b8a1be75745496a8217d5018fd434e1eea
Mwansa Chilekwa <mwansachille@gmail.com>
2022-09-06 22:59:43 +0200

    Remove setAttribute property

--- js/script.js
+++ js/script.js
@@ -41,42 +41,42 @@ function celsius() {
   let temperature = parseInt(celsiusInput.value)
   let celsius = temperature
   let kelvin = celsius + 273.15
-  kelvinTemp.setAttribute('value', Math.round(kelvin * 1000) / 1000)
+  kelvinTemp.value = Math.round(kelvin * 1000) / 1000
   let fahrenheit = 1.8 * celsius + 32
-  fahrenheitInput.setAttribute('value', fahrenheit)
+  fahrenheitInput.value = Math.round(fahrenheit * 1000) / 1000
   let newton = celsius * (33 / 100)
-  newtonInput.setAttribute('value', Math.round(newton * 1000) / 1000)
+  newtonInput.value = Math.round(newton * 1000) / 1000
 }
 
 function kelvin() {
   let temperature = parseInt(kelvinTemp.value)
   let kelvin = temperature
   let celsius = kelvin - 273.15
-  celsiusInput.setAttribute('value', Math.round(celsius * 1000) / 1000)
+  celsiusInput.value = Math.round(celsius * 1000) / 1000
   let fahrenheit = 1.8 * (kelvin - 273.15) + 32
-  fahrenheitInput.setAttribute('value', Math.round(fahrenheit * 1000) / 1000)
+  fahrenheitInput.value = Math.round(fahrenheit * 1000) / 1000
   let newton = (kelvin - 273.15) * (33 / 100)
-  newtonInput.setAttribute('value', Math.round(newton * 1000) / 1000)
+  newtonInput.value = Math.round(newton * 1000) / 1000
 }
 
 function fahrenheit() {
   let temperature = parseInt(fahrenheitInput.value)
   let fahrenheit = temperature
   let celsius = (fahrenheit - 32) / 1.8
-  celsiusInput.setAttribute('value', Math.round(celsius * 1000) / 1000)
+  celsiusInput.value = Math.round(celsius * 1000) / 1000
   let kelvin = (fahrenheit + 459.67) * (5 / 9)
-  kelvinTemp.setAttribute('value', Math.round(kelvin * 1000) / 1000)
+  kelvinTemp.value = Math.round(kelvin * 1000) / 1000
   let newton = (fahrenheit - 32) * (11 / 60)
-  newtonInput.setAttribute('value', Math.round(newton * 1000) / 1000)
+  newtonInput.value = Math.round(newton * 1000) / 1000
 }
 
 function newton() {
   temperature = newtonInput.value
   let newton = temperature
   let celsius = (100 / 33) * temperature
-  celsiusInput.setAttribute('value', Math.round(celsius * 1000) / 1000)
+  celsiusInput.value = Math.round(celsius * 1000) / 1000
   let kelvin = temperature * (100 / 33) + 273
-  kelvinTemp.setAttribute('value', Math.round(kelvin * 1000) / 1000)
+  kelvinTemp.value = Math.round(kelvin * 1000) / 1000
   let fahrenheit = temperature * (60 / 11) + 32
-  fahrenheitInput.setAttribute('value', Math.round(fahrenheit * 1000) / 1000)
+  fahrenheitInput.value = Math.round(fahrenheit * 1000) / 1000
 }
